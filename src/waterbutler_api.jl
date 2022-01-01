b449de2926f864591253d2ec4f06c34104339f1a
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    allow upload_file() to update existing

--- src/waterbutler_api.jl
+++ src/waterbutler_api.jl
@@ -1,6 +1,6 @@
 # Docs: https://waterbutler.readthedocs.io/
 
-function update_file(osf::Client, file::Entity{:files}, content)
+function upload_file(osf::Client, file::Entity{:files}, content)
     @assert file.attributes[:kind] == "file"
     r = HTTP.request("PUT", file.links[:upload], headers(osf), content)
 end
@@ -9,3 +9,5 @@ function upload_file(osf::Client, dir::Entity{:files}, name::String, content)
     @assert dir.attributes[:kind] == "folder"
     r = HTTP.request("PUT", joinpath(dir.links[:upload], "?kind=file&name=$name"), headers(osf), content)
 end
+
+const update_file = upload_file
